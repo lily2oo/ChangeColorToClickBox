5634d3fe12babaa805541213fe0bcdd2afbbe2c0
lily2oo <jumpei102113@icloud.com>
2023-04-27 20:38:11 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const picker = document.getElementById("colorpicker")
+const text = document.getElementById("title")
+const colorcode = document.getElementById("colorcode")
+
+picker.addEventListener('change',()=>{
+    let inputValue = picker.value
+    document.body.style.backgroundColor = inputValue
+    colorcode.innerText = picker.value
+    text.style.color = "#ffffff"
+    colorcode.style.color = "#ffffff"
+})
