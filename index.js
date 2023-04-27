5f88c73c9a4b6955e1bc6a7182f7c5ebdaa976e3
lily2oo <jumpei102113@icloud.com>
2023-04-27 20:53:40 +0900

    ⚡️ change to input

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const picker = document.getElementById("colorpicker")
 const text = document.getElementById("title")
 const colorcode = document.getElementById("colorcode")
 
-picker.addEventListener('change',()=>{
+picker.addEventListener('input',()=>{
     let inputValue = picker.value
     document.body.style.backgroundColor = inputValue
     colorcode.innerText = picker.value
